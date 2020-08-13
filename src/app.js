dcb95815319801e9741c150a6029845dbcb3eb0b
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-08-13 13:05:27 -0700

    add get and push endpoints workin

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -12,17 +13,76 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption));
+app.use(express.json());
 app.use(helmet());
 app.use(cors());
+
 
 const addresses = [];
 
 app.get('/address', (req, res) => {
-    
+    res.json(addresses);
 });
 
 app.post('/address', (req, res) => {
+    const { firstName, lastName, address1, address2, city, state, zip } = req.body;
 
+    // validation code here
+    if (!firstName) {
+        return res
+        .status(400)
+        .send('firstName required');
+    }
+    if (!lastName) {
+        return res
+        .status(400)
+        .send('lastName required');
+    }
+    if (!address1) {
+        return res
+        .status(400)
+        .send('address1 required');
+    }
+    if (!city) {
+        return res
+        .status(400)
+        .send('city required');
+    }
+    if (!state) {
+        return res
+        .status(400)
+        .send('state required');
+    }
+    if (!zip) {
+        return res
+        .status(400)
+        .send('zip required');
+    }
+    if (state.length !== 2) {
+        return res
+          .status(400)
+          .send('State must be 2 characters');
+    }
+    if (zip.length !== 5) {
+        return res
+          .status(400)
+          .send('Zipcode must be 5 characters');
+    }
+
+    const id = uuid();
+    const newAddress = {
+        id,
+        firstName,
+        lastName,
+        address1,
+        address2,
+        city,
+        state,
+        zip
+    };
+
+    addresses.push(newAddress);
+    res.status(201).location(`http://localhost:8000/address/${id}`).json(newAddress);
 });
 
 app.delete('/address/:addressId', (req, res) => {
